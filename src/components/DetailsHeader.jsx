e74ae29f45dd99775f86fd33b14cfdd7631d7586
Santosh kumar <sumitsantosh555@gmail.com>
2022-11-25 15:46:52 +0530

    song detail

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -1,19 +1,34 @@
 import { Link } from "react-router-dom";
 
-const DetailsHeader = ({ artistId, artistData, songData}) => (
+const DetailsHeader = ({ artistId, artistData, songData}) => {
 
-
+  const artist = artistData?.artists[artistId]?.attributes;
+return (
   <div className="w-full relative flex flex-col">
      <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28" />
 
      <div className="absolute inset-0 flex items-center">
         <img
         alt="art"
-         src= {artistId ? artistData?.artists[artistId].attributes?.artwork?.url.replace('{w}', '500').replace('{h}', '500') : songData?.images?.coverart}
+         src= {artistId ? artist?.artwork?.url.replace('{w}', '500').replace('{h}', '500') : songData?.images?.coverart}
          className="sm:w-48 w-28 sm:h-48 h-28 rounded-full object-cover border-2 shadow-xl shadow-black"
        />
+
+       <div className="ml-5">
+        <p className="font-bold sm:text-3xl text-xl text-white">
+        {artistId ? artist?.name : songData?.title}</p>
+        {!artistId && (
+          <Link to={`/artists/${songData?.artists[0].adamid}`}>
+            <p className="text-base text-gray-400 mt-2">
+              {songData?.subtitle}
+            </p>
+          </Link>
+        )}
+       </div>
      </div>
+
   </div>
-);
+)
+}
 
 export default DetailsHeader;
