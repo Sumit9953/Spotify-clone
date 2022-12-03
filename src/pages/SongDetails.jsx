e065e548462ccc8a93f0e1feb8157f30a0c2765a
Santosh kumar <sumitsantosh555@gmail.com>
2022-12-03 19:13:42 +0530

    complete detail2

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { DetailsHeader, Error , Loader, RelatedSongs } from "../components";
 import { setActiveSong, playPause } from "../redux/features/playerSlice";
 
-import { useGetSongDetailsQuery } from "../redux/services/shazamCore";
+import { useGetSongDetailsQuery, useGetSongRelatedQuery } from "../redux/services/shazamCore";
  
 const SongDetails = () => {
     const dispatch = useDispatch();
@@ -11,6 +11,20 @@ const SongDetails = () => {
     const {activeSong, isPlaying } = useSelector((state) => state.player)
 
     const {data: songData, isFetching: isFetchingSongDetails} = useGetSongDetailsQuery({songid}); 
+    const {data, isFetching: isFetchingRelatedSongs , error} = useGetSongRelatedQuery({songid});
+
+    const handlePauseClick = () => {
+        dispatch(playPause(false));
+      }
+      const handlePlayClick = (song,i) => {
+        dispatch(setActiveSong({ song,data,i}));
+        dispatch(playPause(true));
+      }
+
+    if(isFetchingSongDetails || isFetchingRelatedSongs){
+        return <Loader title="Searching song details" />;
+    }
+    if(error) return <Error />
 
     console.log(songid);
 
@@ -28,6 +42,13 @@ return (
             ) ): <p className="text-gray-400 text-base my-1">Sorry, No Lyrics Found.</p>}
         </div>
     </div>
+    <RelatedSongs
+    data = {data}
+    isPlaying = {isPlaying}
+    activeSong = {activeSong}
+    handlePauseClick={handlePauseClick}
+    handlePlayClick = {handlePlayClick}
+    />
 </div>
 
 )
