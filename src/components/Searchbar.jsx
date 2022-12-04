586e83b33f5fb0546966094f6ca84a4a97d8f31d
Santosh kumar <sumitsantosh555@gmail.com>
2022-12-04 15:20:08 +0530

    compelet

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -2,8 +2,19 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FiSearch } from 'react-icons/fi'
 
-const Searchbar = () => (
-  <form autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600 " >
+const Searchbar = () => {
+
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    navigate(`/search/${searchTerm}`)
+  }
+
+  return (
+  <form onSubmit={handleSubmit} autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600 " >
     <label htmlFor="search-filed" className="sr-only">
       Search all songs
     </label>
@@ -14,13 +25,15 @@ const Searchbar = () => (
       autoComplete="off"
       id="search_field"
       placeholder="Search"
-      value=""
+      value={searchTerm}
       type="search" 
-      onChange={() => {}}
+      onChange={(e) => setSearchTerm(e.target.value)}
       className="flex-1 bg-transparent border-none outline-none placeholder-gray-500 text-base text-white p-4"
       />
     </div>
   </form>
 );
+
+}
 
 export default Searchbar;
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -17,7 +17,8 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
           getSongDetails: builder.query({ query: ({songid}) => `/tracks/details?track_id=${songid}` }),
           getSongRelated: builder.query({ query: ({songid}) => `/tracks/related?track_id=${songid}`}),
           getArtistDetails: builder.query({ query: ({artistId}) => `/artists/details?artist_id=${artistId}`}),
-          getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}`})
+          getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}`}),
+          getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`})
         }), 
     })
 
@@ -28,4 +29,5 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
       useGetSongRelatedQuery,
       useGetArtistDetailsQuery,
       useGetSongsByCountryQuery,
+      useGetSongsBySearchQuery
     } = shazamCoreApi;
